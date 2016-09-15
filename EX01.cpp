d28c59966ed60a6074546bcab99d1bdeafe65ab4
sjohnstone18 <sjohnstone18@LAB-SCI-214-15>
2016-09-15 11:52:24 -0700

    ex3
    commented char functions and cleaned up others

--- EX01.cpp
+++ EX01.cpp
@@ -1,22 +1,24 @@
 // ConsoleApplication1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "stdbool.h"
 #include "stdlib.h"
 #include "ctime"
 #include "iostream"
+#include "string"
+#include "math.h"
 using namespace std;
 
 
 void ex02();
-
+void ex03();
 ///common
 
 int main()
 {
 	ex02();
-
+	ex03();
 	return 0;
 }
 
@@ -39,11 +41,50 @@ void ex02()
 	cin >> boxWidth;
 	cout << "\n please enter book width\n";
 	cin >> bookwidth;
-	if (boxWidth % bookwidth = 0)
+	if (boxWidth % bookwidth == 0 || bookwidth % boxWidth == 0)
 	{
 		cout << "\nThe widths are evenly divible\n";
 	}
 	else
 		cout << "\nThey are not divisible\n";
-	return 0;
+
 }
+
+void ex03()
+{
+	int aSquare;
+	cout << " please enter the area of a square\n";
+	cin >> aSquare;
+	int diag;
+	diag = sqrt(2 * aSquare);
+	cout << "\n The diagonal of the square is " << diag << " units long\n";
+
+	//part b
+	/*char yn;
+	cout << "please enter y or n\n";
+	cin >> "yn";
+
+	if (yn == 'y')
+		cout << "yes\n";
+	else if (yn == 'n')
+		cout << "no\n";
+	else
+		cout << "thats wrong :(\n";
+
+
+	//part c
+	//char tab;
+	//tab = '\t';
+	*/
+
+	string mailingAddress;
+	cout << "please enter your mailing address\n";
+	cin >> mailingAddress;
+
+	string stringV;
+
+
+}
+
+
+
