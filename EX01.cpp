eb25cb3b07ca60c25a8fbc453c35ecfed6f4c8cc
sjohnstone18 <sjohnstone18@LAB-SCI-214-15>
2016-09-15 15:24:04 -0700

    update
    compleedpart 1 and 2. some output typos

--- EX01.cpp
+++ EX01.cpp
@@ -13,6 +13,8 @@ using namespace std;
 
 void ex02();
 void ex03();
+void ex04();
+void ex05();
 ///common
 
 int main()
@@ -30,46 +32,59 @@ void ex02()
 	
 	//part 1a
 	bool hasPassedTest = true;
+
 	//part 1b
 	srand(time(NULL));
 	int x = rand();
 	int y = rand();
 	if (x >= y)
-	{
+		{
 		cout << "x is greater than or equal to y\n";
-	}
+		}
 	else
-		cout << "x is less than y";
+		cout << "x is less than y\n";
 	
 	//part 1c
 	cout << "please enter the Number of shares\n";
 	int numberofShares; 
 	cin >> numberofShares;
-
 	if (numberofShares < 100)
 		cout << "you have less than 100 shares\n";
 	else
 		cout << "you have mre than 100 shares\n";
 
-
-	
 	//part 1d
 	cout << "please enter box width\n";
-
 	cin >> boxWidth;
 	cout << "\nplease enter book width\n";
 	cin >> bookwidth;
 	if (boxWidth % bookwidth == 0 || bookwidth % boxWidth == 0)
-	{
-		cout << "\nThe widths are evenly divible\n";
-	}
+		{
+			cout << "\nThe widths are evenly divible\n";
+		}	
 	else
 		cout << "\nThey are not divisible\n";
+
+	//part 1e
+	cout << "please enter thwe shelf life of the chocolate: ";
+	int chocLife;
+	cin >> chocLife;
+	cout << "\nplease enter outside temperature in Farenheit: ";
+	int temperature;
+	cin >> temperature;
+	if (temperature > 90)
+			{
+				chocLife = chocLife - 4;
+				cout << "\nthe shelf life of the chocloate is now " << chocLife << endl;
+			}
+	else
+				cout << "\nthe shelf life of the chocloate is still" << chocLife << endl;
 
 }
 
 void ex03()
 {
+	//part 2a
 	int aSquare;
 	cout << " please enter the area of a square\n";
 	cin >> aSquare;
@@ -79,34 +94,44 @@ void ex03()
 
 	//part b
 	char yn;
-	//int yn;
 	cout << "please enter y or n\n";
 	cin >> yn;
-
 	if (yn == 'y')
 		cout << "yes\n";
 	else if (yn == 'n')
 		cout << "no\n";
 	else
 		{
-		cout << "thats wrong :(\n";
+			cout << "thats wrong :(\n";
 		};
-		
+
 
 	//part c
-	//char tab;
-	//tab = '\t';
+	char tab;
+	tab = '\t';
 	
-
+	//part 2d
 	string mailingAddress;
 	cout << "please enter your mailing address\n";
 	cin >> mailingAddress;
-
 	cout << "your mailing address is " << mailingAddress << endl;
-	string stringV;
+	
+	//part2e
+	string stringV = "";
+}
+
+void ex04()
+{
+	//part3a
 
 
+	
 }
 
 
 
+void ex05()
+{
+
+
+}
