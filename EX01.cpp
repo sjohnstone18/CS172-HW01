74f97d42259c8a96b5ad0d433520a03ccce62eac
sjohnstone18 <sjohnstone18@LAB-SCI-214-15>
2016-09-15 11:20:58 -0700

    added my code

--- EX01.cpp
+++ EX01.cpp
@@ -2,12 +2,47 @@
 //
 
 #include "stdafx.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "ctime"
+#include "iostream"
+using namespace std;
+
+
+void ex02();
 
 
 int main()
 {
-	cout << "hello World!!";
+	ex02();
 
 	return 0;
 }
 
+
+void ex02()
+{
+	int bookwidth;
+	int boxWidth;
+	bool hasPassedTest = true;
+	srand(time(NULL));
+	int x = rand();
+	int y = rand();
+	if (x >= y)
+	{
+		cout << "x is greater than or equal to y\n";
+
+	}
+	cout << "please enter box width\n";
+
+	cin >> boxWidth;
+	cout << "\n please enter book width\n";
+	cin >> bookwidth;
+	if (boxWidth % bookwidth = 0)
+	{
+		cout << "\nThe widths are evenly divible\n";
+	}
+	else
+		cout << "\nThey are not divisible\n";
+	return 0;
+}
